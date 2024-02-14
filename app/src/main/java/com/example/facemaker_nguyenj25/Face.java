76f657cff5ba83441ac0e273570c4e26329886a6
jnguyen1515 <jnguyen1515@gmail.com>
2024-02-13 21:29:57 -0800

    Updated with comments -James 13 feb 2024

--- app/src/main/java/com/example/facemaker_nguyenj25/Face.java
+++ app/src/main/java/com/example/facemaker_nguyenj25/Face.java
@@ -2,7 +2,9 @@ package com.example.facemaker_nguyenj25;
 //@author James Nguyen
 
 import android.graphics.Canvas;
+
 import java.util.Random;
+
 public class Face {
     //private instance variables
     private int skinColor;
@@ -10,33 +12,47 @@ public class Face {
     private int hairColor;
     private int hairStyle;
 
-    public Face(){
+    public Face() {
         random();
     }
     //this randomizes the int to create a random face out of the ints given
-    public void random(){
-        //used geek for geef website for reference
+
+    /**
+     * External Citation
+     * Date: 13 Feb 2024
+     * Problem: Could not remember how to use a random method
+     * <p>
+     * Resource: https://www.geeksforgeeks.org/java-util-random-nextint-java/?ref=header_search
+     * <p>
+     * Solution: I used this link to help give me a refresher on what to do
+     */
+    public void random() {
         Random randomize = new Random();
         skinColor = randomize.nextInt();
         eyeColor = randomize.nextInt();
         hairColor = randomize.nextInt();
         hairStyle = randomize.nextInt();
     }
+
     //this is a method to draw the Face on a canvas
-    public void onDraw(Canvas canvas){
+    public void onDraw(Canvas canvas) {
 
     }
+
     //bunch of getter methods
-    public int getSkinColor(){
+    public int getSkinColor() {
         return skinColor;
     }
-    public int getEyeColor(){
+
+    public int getEyeColor() {
         return eyeColor;
     }
-    public int getHairColor(){
+
+    public int getHairColor() {
         return hairColor;
     }
-    public int getHairStyle(){
+
+    public int getHairStyle() {
         return hairStyle;
     }
 }
--- app/src/main/java/com/example/facemaker_nguyenj25/MainActivity.java
+++ app/src/main/java/com/example/facemaker_nguyenj25/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.facemaker_nguyenj25;
 //@author James Nguyen
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,24 +17,25 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //used android std web for help
+        // Initialize the spinner and populate it with the hair options from resources
         Spinner spinner = findViewById(R.id.hair_spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.hair_array, android.R.layout.simple_spinner_item);
 
+        // Set the layout for the drop-down items
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        // Set the adapter for the spinner
         spinner.setAdapter(adapter);
     }
 
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(this, "Selected Item" + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Selected Item: " + parent.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
-
+        // This method is called when nothing is selected in the spinner
     }
-
 }

13d3e02bdbca55cf1c409cf56e92e6baffc997bf
jnguyen1515 <jnguyen1515@gmail.com>
2024-02-19 15:55:11 -0800

    Trying to figure out how to set colors and draw -James 13 feb 2024

--- app/src/main/java/com/example/facemaker_nguyenj25/Face.java
+++ app/src/main/java/com/example/facemaker_nguyenj25/Face.java
@@ -1,22 +1,25 @@
 package com.example.facemaker_nguyenj25;
 //@author James Nguyen
 
+import android.content.Context;
 import android.graphics.Canvas;
+import android.view.SurfaceView;
 
 import java.util.Random;
 
-public class Face {
+public class Face extends SurfaceView {
     //private instance variables
     private int skinColor;
     private int eyeColor;
     private int hairColor;
     private int hairStyle;
 
-    public Face() {
-        random();
+    public Face(Context context) {
+        super(context);
     }
-    //this randomizes the int to create a random face out of the ints given
 
+
+    //this randomizes the int to create a random face out of the ints given
     /**
      * External Citation
      * Date: 13 Feb 2024
@@ -36,7 +39,7 @@ public class Face {
 
     //this is a method to draw the Face on a canvas
     public void onDraw(Canvas canvas) {
-
+        canvas.drawCircle(200.0f, 300.0f, 50.0f, // color needed);
     }
 
     //bunch of getter methods

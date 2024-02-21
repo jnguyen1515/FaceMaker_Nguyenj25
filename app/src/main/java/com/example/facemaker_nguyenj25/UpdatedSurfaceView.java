c59d232532664c78df1a80fe732d4b6ea757335e
jnguyen1515 <jnguyen1515@gmail.com>
2024-02-21 14:54:34 -0800

    Create a updated surface view to update it with drawings

---
+++ app/src/main/java/com/example/facemaker_nguyenj25/UpdatedSurfaceView.java
@@ -0,0 +1,10 @@
+package com.example.facemaker_nguyenj25;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class UpdatedSurfaceView extends SurfaceView {
+    public UpdatedSurfaceView(Context context) {
+        super(context);
+    }
+}
